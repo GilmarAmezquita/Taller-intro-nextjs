825a976ea14b3c4b92318469ed8ca7dc8984b724
Jhan Carlos Carvajal Bastidas <jan.carvajal1@u.icesi.edu.co>
2022-04-24 23:27:19 -0500

    I change the index.js to use it as the log in screen. Also, I add two new classes Home, it will be use when a user is logged in, and sign up screen to register a new user. I am working on that screens design

---
+++ exams-platform/pages/signUp.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+
+const SignUp = () => {
+    return (
+        <main className={styles.main}>
+        <h1 className={styles.title}>
+          Sign <a>Up</a>
+        </h1>
+      </main>
+    )
+}
+
+export default SignUp
