006327f333a1872ddfed1a3cb20b72d459817b56
Jhan Carlos Carvajal Bastidas <jan.carvajal1@u.icesi.edu.co>
2022-04-25 00:17:59 -0500

    finished the design of the sign up screen

--- exams-platform/pages/signUp.js
+++ exams-platform/pages/signUp.js
@@ -1,11 +1,27 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
 const SignUp = () => {
     return (
-        <main className={styles.main}>
+        <main className={styles.main} >
         <h1 className={styles.title}>
           Sign <a>Up</a>
         </h1>
+
+        <div className={styles.grid}>
+            <input placeholder="Name" className={styles.card}></input>
+            <input placeholder="Lastname" className={styles.card}></input>
+        </div>
+        <p>Role</p>
+        <div className={styles.grid}>
+            Teacher<input type="radio" name="role" className={styles.card}></input>
+            Student<input type="radio" name="role" className={styles.card}></input>
+        </div>
+
+        <div className={styles.grid}>
+                <input placeholder="Username" className={styles.card}></input>
+                <input placeholder="password" className={styles.card}></input>
+        </div>
+        <button className={styles.card}>Register</button>
       </main>
     )
 }
