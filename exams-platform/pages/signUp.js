a7d206fdb28faa99ba8023412a58b5dce62774f3
Jhan Carlos Carvajal Bastidas <jan.carvajal1@u.icesi.edu.co>
2022-04-25 19:58:36 -0500

    added an advance of the sign up screem form

--- exams-platform/pages/signUp.js
+++ exams-platform/pages/signUp.js
@@ -1,28 +1,49 @@
 import styles from '../styles/Home.module.css';
 
+
+let state = {
+  name: "",
+  lastname: "",
+  teacher: false,
+  student: false,
+  username: "",
+  password: ""
+}
+
+let handleChage = e => {
+  switch (e.target.name) {
+    case "name": state.name = e.target.value; 
+    case "lastname": state.lastname = e.target.value
+    case "teacher": state.teacher = e.target.value
+    case "student": state.student = e.target.value
+    case "username": state.username = e.target.value
+    case "password": state.username = e.target.value
+  }
+}
+
 const SignUp = () => {
     return (
-        <main className={styles.main} >
+        <form className={styles.main} >
         <h1 className={styles.title}>
           Sign <a>Up</a>
         </h1>
 
         <div className={styles.grid}>
-            <input placeholder="Name" className={styles.card}></input>
-            <input placeholder="Lastname" className={styles.card}></input>
+            <input placeholder="Name" className={styles.card} name="name"></input>
+            <input placeholder="Lastname" className={styles.card} name="lastname"></input>
         </div>
         <p>Role</p>
         <div className={styles.grid}>
-            Teacher<input type="radio" name="role" className={styles.card}></input>
-            Student<input type="radio" name="role" className={styles.card}></input>
+            Teacher<input type="radio" className={styles.card} name="role" id="teacher"></input>
+            Student<input type="radio" className={styles.card} name="role" id="student"></input>
         </div>
 
         <div className={styles.grid}>
-                <input placeholder="Username" className={styles.card}></input>
-                <input placeholder="password" className={styles.card}></input>
+                <input placeholder="Username" className={styles.card} name="username"></input>
+                <input placeholder="password" className={styles.card} name="password"></input>
         </div>
-        <button className={styles.card}>Register</button>
-      </main>
+        <input type="submit" value="register"/>
+      </form>
     )
 }
 
