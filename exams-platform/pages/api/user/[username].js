bc10e25b5aebb253edade20c6c6ed282cdedf40a
Gilmar Amezquita <gilmarandresamezquita@hotmail.com>
2022-05-01 21:19:28 -0500

    User query corrected

--- exams-platform/pages/api/user/[username].js
+++ exams-platform/pages/api/user/[username].js
@@ -3,7 +3,7 @@ import {db} from '../../../data/database';
 export default async (req, res) => {
     const {method, body} = req;
     if(method == 'GET'){
-        const query = "SELECT * FROM users WHERE u.username='"+req.query.username+"'";
+        const query = "SELECT * FROM USERS WHERE USERNAME = '"+req.query.username+"'";
         const response = await db.query(query);
         if(response.rows[0] == undefined){
             return res.status(400).json({username: req.query.username});
