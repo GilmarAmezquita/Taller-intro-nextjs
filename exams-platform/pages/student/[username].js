b4972522b61e86822f8ab233902eb9abb030609f
Gilmar Amezquita <gilmarandresamezquita@hotmail.com>
2022-05-02 02:03:09 -0500

    Added new files

---
+++ exams-platform/pages/student/[username].js
@@ -0,0 +1,28 @@
+import {useRouter} from 'next/router'
+
+function studentHome ({data}) {
+    const router = useRouter();
+    const userName = router.query.username;
+    return(
+        <div>
+            <div>
+                <h1>Hello student {userName}</h1>
+            </div>
+        </div>
+    )
+}
+
+studentHome.getInitialProps = async (req, res) => {
+    let config = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }
+    const response = await fetch('http://localhost:3000/api/users/' + req.query.username, config);
+    const data = await response.json();
+    return { data };
+}
+
+export default studentHome
---
+++ exams-platform/pages/teacher/[username].js
@@ -0,0 +1,15 @@
+import {useRouter} from 'next/router'
+
+const teacherHome = () =>{
+    const router = useRouter();
+    const userName = router.query.username;
+    return(
+        <div>
+            <div>
+                <h1>Hello teacher {userName}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default teacherHome
