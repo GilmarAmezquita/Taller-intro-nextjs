3cb5eb5aba18a6b2782fdc5f1a4018bb8b09916e
Gilmar Amezquita <gilmarandresamezquita@hotmail.com>
2022-05-05 14:09:59 -0500

    Updated authentication

--- exams-platform/pages/api/user/[username].js
+++ exams-platform/pages/api/user/[username].js
@@ -1,16 +1,22 @@
 import {db} from '../../../data/database';
 
 export default async (req, res) => {
-    const {method, body} = req;
-    if(method == 'GET'){
-        const query = "SELECT * FROM USERS WHERE USERNAME = '"+req.query.username+"'";
-        const response = await db.query(query);
-        if(response.rows[0] == undefined){
-            return res.status(400).json({username: req.query.username});
-        }else{
-            return res.status(200).json(response.rows[0]);
-        }
-    }else{
-        return res.status(400).json('Metodo no permitido');
+    switch (req.method){
+        case 'GET': 
+            res.statusCode = 404; 
+            break;
+        case 'POST':
+            const response = await db.query('SELECT * FROM USERS WHERE USERNAME = $1 AND PASSWORD = $2', [req.body.username, req.body.password]);
+            if(response.rows && response.length > 0){
+                res.json({
+                    flag: true,
+                    nickname: response.rows[0].username
+                })
+            }else{
+                res.json({
+                    flag: false
+                })
+            }
+            break;
     }
 }
